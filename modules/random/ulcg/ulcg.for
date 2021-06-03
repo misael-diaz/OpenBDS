d315f6c63a1c8e0c8c0528848f032191dfe2ba30
misael-diaz <misael.diaz@upr.edu>
2021-06-03 09:27:50 -0400

    adds licensing info in source file

---
+++ modules/random/ulcg/ulcg.for
@@ -0,0 +1,63 @@
+!
+!
+! / ===================================================================== \
+!  NOTICE:
+!  This Work is derived from the TestU01 Package. Translates portions of
+!  the following source files to FORTRAN: unif01.c and ulcg.c. Names of
+!  variables and functions have been preserved to some extent.
+! \ ===================================================================== /
+!
+!
+!
+!
+!  Copyright (c) 2002 Pierre L'Ecuyer, DIRO, Université de Montréal.
+!  e-mail: lecuyer@iro.umontreal.ca
+!  All rights reserved.
+!
+!  Redistribution and use in source and binary forms, with or without
+!  modification, are permitted without a fee for private, research,
+!  academic, or other non-commercial purposes.
+!  Any use of this software in a commercial environment requires a
+!  written licence from the copyright owner.
+!
+!  Any changes made to this package must be clearly identified as such.
+!
+!  In scientific publications which used this software, a reference to it
+!  would be appreciated.
+!
+!  Redistributions of source code must retain this copyright notice
+!  and the following disclaimer.
+!
+!  THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR
+!  IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+!  WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+!
+!
+!
+!  Derived Work
+!
+!  source: ulcg.for
+!  author: misael-diaz
+!  date:   2021/06/02
+!
+!
+!  Synopsis:
+!  Defines linear congruential generators of pseudo-random numbers.
+!
+!
+!  Copyright (c) 2021 Misael Diaz-Maldonado
+!  All rights reserved.
+!
+!  Licensed under the Apache License, Version 2.0 (the "License");
+!  you may not use this file except in compliance with the License.
+!  You may obtain a copy of the License at
+!
+!    http://www.apache.org/licenses/LICENSE-2.0
+!
+!  Unless required by applicable law or agreed to in writing, software
+!  distributed under the License is distributed on an "AS IS" BASIS,
+!  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+!  See the License for the specific language governing permissions and
+!  limitations under the License.
+!
+!
