c971f5aad2d8b19e4d232c0c95edccff04a030a3
misael-diaz <misael.diaz@upr.edu>
2021-06-18 10:05:07 -0400

    assigns random values to vectors

--- tests/modules/math/vector/test_math_vector.for
+++ tests/modules/math/vector/test_math_vector.for
@@ -28,11 +28,18 @@ program test_math_vector_class
     use math_vector_class, only: math_vector_t => vector_t
     implicit none
 
+
     type(math_vector_t), pointer :: vector => null()
-    
+
+
+    real(kind = real64), allocatable :: d(:)
+
+
     integer(kind = int64) :: i
     integer(kind = int64), parameter :: n = 65536_int64
     integer(kind = int32) :: mstat = 0
+
+
 
 
     allocate(vector, stat = mstat)
@@ -40,18 +47,57 @@ program test_math_vector_class
 
 
     vector = math_vector_t (n)  ! instantiation
-    vector % x = 1.0_real64
-    vector % y = 1.0_real64
-    vector % z = 1.0_real64
+
+
+    do i = 1, n
+        call random_number  ( vector % x(i) )
+    end do
+
+    do i = 1, n
+        call random_number  ( vector % y(i) )
+    end do
+
+    do i = 1, n
+        call random_number  ( vector % z(i) )
+    end do
+
 
 
     call vector % normalize ()
 
 
     do i = 1, n
-        print *, vector % x(i), vector % y(i), vector % z(i)
+        print '(1X,3F8.4)', vector % x(i), vector % y(i), vector % z(i)
     end do
 
+
+
+
+
+    allocate (d(n), stat = mstat)
+    if (mstat /= 0) error stop "allocation failure"
+
+
+
+    do i = 1, n
+        d(i) = vector % range (n, i)     ! computes vector distance
+    end do
+
+
+    print *, ""
+    print *, ""
+    print *, "distance::min: ", minval(d)
+    print *, "distance::max: ", maxval(d)
+    print *, ""
+    print *, ""
+
+
+
+
+
+
+    deallocate (d, stat = mstat)
+    if (mstat /= 0) error stop "unexpected deallocation failure"
 
     deallocate (vector, stat = mstat)
     if (mstat /= 0) error stop "unexpected deallocation failure"
@@ -70,3 +116,10 @@ end program
 ! upon instantiation when it is declared with the allocatable attribute.
 ! To avoid this (possible) reallocation it's declared with the pointer 
 ! attribute.
+!
+!
+! Vectorization:
+! The Intel Fortran Compiler vectorizes the random_number() intrinsic. Other
+! than that the GNU Fortran Compiler vectorizes everything else its Intel
+! counterpart does. I did not perform a runtime check, just checked the
+! optimizer reports of both compilers.
