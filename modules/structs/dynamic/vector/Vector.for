df889225589c23ddf9202667603d640352ec4917
misael-diaz <misael.diaz@upr.edu>
2021-06-20 19:42:10 -0400

    makes vector components allocatable

--- modules/structs/dynamic/vector/Vector.for
+++ modules/structs/dynamic/vector/Vector.for
@@ -5,7 +5,7 @@
 !
 !
 !   Synopsis:
-!   Defines the vector class.
+!   Defines the (dynamic) vector class.
 !
 !
 !   Copyright (C) 2021 Misael Diaz-Maldonado
@@ -23,11 +23,47 @@
 !   You should have received a copy of the GNU General Public License
 !   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+module idata
+    ! Defines the type i[nternal]data of the vector class
+    use, intrinsic :: iso_fortran_env, only: int32, int64
+    use utils, only: deallocator => util_deallocate_array
+    implicit none
+    private
+
+
+    type, public :: data_t
+        integer(kind = int64), allocatable :: values_int64_t(:)
+        integer(kind = int32), allocatable :: values_int32_t(:)
+        contains
+            private
+            final :: finalizer
+    end type
+
+
+    contains
+
+        subroutine finalizer (data)
+            type(data_t), intent(inout) :: data
+
+!           print *, "destroying dynamic vector data ... "
+
+            call deallocator (data % values_int64_t)
+            call deallocator (data % values_int32_t)
+
+            return
+        end subroutine
+
+end module idata
+
+
 module vectors
     use, intrinsic :: iso_fortran_env, only: int32, int64
-    use utils, only: allocator   => util_allocate_array
+    use utils, only: util_allocate_array_int32_by_bounds
+    use utils, only: util_allocate_array_int64_by_bounds
     use utils, only: reallocator => util_reallocate_array
-    use utils, only: deallocator => util_deallocate_array
+    use utils, only: util_deallocate_array_int32
+    use utils, only: util_deallocate_array_int64
+    use idata, only: data_t
     implicit none
     private
 
@@ -37,24 +73,18 @@ module vectors
     end type
 
 
-    type :: data_t
-        integer(kind = int32), allocatable :: values_int32_t(:)
-        integer(kind = int64), allocatable :: values_int64_t(:)
-    end type
-
-
     type :: stat_t
-        logical(kind = int32) :: init = .false.
+        logical(kind = int64) :: init = .false.
     end type
 
 
     type, public :: vector_t
         private
-        type(iter_t):: begin
-        type(iter_t):: avail
-        type(iter_t):: limit
-        type(data_t):: array
-        type(stat_t):: state
+        type(iter_t), allocatable :: begin
+        type(iter_t), allocatable :: avail
+        type(iter_t), allocatable :: limit
+        type(data_t), allocatable :: array
+        type(stat_t), allocatable :: state
         contains
             private
             procedure :: addressing_method
@@ -72,6 +102,25 @@ module vectors
     end interface
 
 
+    interface allocator
+        module procedure allocate_iter_t
+        module procedure allocate_data_t
+        module procedure allocate_stat_t
+        module procedure allocate_vector_t
+        module procedure util_allocate_array_int32_by_bounds
+        module procedure util_allocate_array_int64_by_bounds
+    end interface
+
+
+    interface deallocator
+        module procedure deallocate_iter_t
+        module procedure deallocate_data_t
+        module procedure deallocate_stat_t
+        module procedure util_deallocate_array_int32
+        module procedure util_deallocate_array_int64
+    end interface
+
+
     interface
 
 
@@ -79,6 +128,11 @@ module vectors
             ! Synopsis: Returns an empty vector
             type(vector_t):: vector
         end function
+
+
+        module subroutine instantiate (vector)
+            type(vector_t), intent(inout) :: vector
+        end subroutine
 
 
         module function findloc_method (self, value) result(idx)
@@ -149,12 +203,64 @@ module vectors
         end subroutine
 
 
+        module subroutine allocate_vector_t (v)
+            type(vector_t), intent(inout) :: v
+        end subroutine
+
+
+        module subroutine allocate_iter_t (i)
+            type(iter_t), intent(inout), allocatable :: i
+        end subroutine
+
+
+        module subroutine allocate_data_t (d)
+            type(data_t), intent(inout), allocatable :: d
+        end subroutine
+
+
+        module subroutine allocate_stat_t (s)
+            type(stat_t), intent(inout), allocatable :: s
+        end subroutine
+
+
+        module subroutine deallocate_iter_t (i)
+            type(iter_t), intent(inout), allocatable :: i
+        end subroutine
+
+
+        module subroutine deallocate_data_t (d)
+            type(data_t), intent(inout), allocatable :: d
+        end subroutine
+
+
+        module subroutine deallocate_stat_t (s)
+            type(stat_t), intent(inout), allocatable :: s
+        end subroutine
+
+
+        module subroutine is_empty (vector)
+            type(vector_t), intent(in) :: vector
+        end subroutine
+
+
+        module subroutine is_instantiated (vector)
+            type(vector_t), intent(inout) :: vector
+        end subroutine
+
+        module subroutine check_bounds (vector, idx)
+            type(vector_t), intent(in) :: vector
+            integer(kind = int64), intent(in) :: idx
+        end subroutine
+
+
         module subroutine finalizer (vector)
             type(vector_t), intent(inout) :: vector
         end subroutine
 
 
     end interface
+
+
 end module
 
 
