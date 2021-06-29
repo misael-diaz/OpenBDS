e8834b85aa8c1d8fe9812525732baeb258a9dc19
misael-diaz <misael.diaz@upr.edu>
2021-06-29 09:27:43 -0400

    guards against pushing mixed-type objects

--- modules/structs/dynamic/vector/submodules/Vector_int64_t_implementations.for
+++ modules/structs/dynamic/vector/submodules/Vector_int64_t_implementations.for
@@ -1,5 +1,5 @@
 !
-!   source: Vector_int32_t_implementations.for
+!   source: Vector_int64_t_implementations.for
 !   author: misael-diaz
 !   date:   2021-06-27
 !
@@ -23,16 +23,16 @@
 !   You should have received a copy of the GNU General Public License
 !   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-submodule (vectors) vectors_int32_t_implementation
+submodule (vectors) vectors_int64_t_implementation
     contains
 
 
-        module subroutine vector_int32_t_findloc_wrapper (vector, value, i)
+        module subroutine vector_int64_t_findloc_wrapper (vector, value, i)
             type(vector_t), intent(in) :: vector
             integer(kind = int64), intent(out) :: i
             integer(kind = int64) :: lb
             integer(kind = int64) :: ub
-            integer(kind = int32), intent(in) :: value
+            integer(kind = int64), intent(in) :: value
 
 
             lb = vector % begin % idx
@@ -42,7 +42,7 @@ submodule (vectors) vectors_int32_t_implementation
             associate (values => vector % array % values)
 
                 select type (values)
-                    type is ( integer(kind = int32) )
+                    type is ( integer(kind = int64) )
                         i= findloc(array = values(lb:ub), &
                                  & value = value, dim = 1, kind = int64)
                     class default
@@ -54,15 +54,15 @@ submodule (vectors) vectors_int32_t_implementation
         end subroutine
 
 
-        module subroutine vector_int32_t_indexer (vector, idx, value)
+        module subroutine vector_int64_t_indexer (vector, idx, value)
             type(vector_t), intent(in) :: vector
             integer(kind = int64), intent(in) :: idx
-            integer(kind = int32), intent(out) :: value
+            integer(kind = int64), intent(out) :: value
 
             associate (values => vector % array % values)
 
                 select type (values)
-                    type is ( integer(kind = int32) )
+                    type is ( integer(kind = int64) )
                         value = values (idx)
                     class default
                         error stop vector % state % errmsg
@@ -75,10 +75,10 @@ submodule (vectors) vectors_int32_t_implementation
         end subroutine
 
 
-        module subroutine vector_int32_t_push_back (vector, value)
-            ! Synopsis: Pushes 32-bit integer unto back of vector.
+        module subroutine vector_int64_t_push_back (vector, value)
+            ! Synopsis: Pushes 64-bit integer unto back of vector.
             type(vector_t), intent(inout) :: vector
-            integer(kind = int32), intent(in) :: value
+            integer(kind = int64), intent(in) :: value
 
 
             call is_instantiated (vector)
@@ -94,10 +94,10 @@ submodule (vectors) vectors_int32_t_implementation
         end subroutine
 
 
-        module subroutine vector_int32_t_insert_back (vector, value)
+        module subroutine vector_int64_t_insert_back (vector, value)
             ! Synopsis: Inserts value unto back, vector grows as needed.
             type(vector_t), intent(inout) :: vector
-            integer(kind = int32), intent(in) :: value
+            integer(kind = int64), intent(in) :: value
 
 
             if (vector % avail % idx == vector % limit % idx) then
@@ -109,7 +109,7 @@ submodule (vectors) vectors_int32_t_implementation
                       values => vector % array % values)
 
                 select type (values)
-                    type is ( integer(kind = int32) )
+                    type is ( integer(kind = int64) )
                         values (avail) = value
                         avail = avail + 1_int64
                     class default
@@ -124,10 +124,10 @@ submodule (vectors) vectors_int32_t_implementation
         end subroutine
 
 
-        module subroutine vector_int32_t_slice (vector, it)
+        module subroutine vector_int64_t_slice (vector, it)
             ! Synopsis: Binds iterator to slice of (pushed values).
             type(vector_t), intent(in), target :: vector
-            integer(kind = int32), intent(inout), &
+            integer(kind = int64), intent(inout), &
                 & pointer, contiguous :: it(:)
             integer(kind = int64) :: lb
             integer(kind = int64) :: ub
@@ -141,7 +141,7 @@ submodule (vectors) vectors_int32_t_implementation
             associate (values => vector % array % values)
 
                 select type (values)
-                    type is ( integer(kind = int32) )
+                    type is ( integer(kind = int64) )
                         it => values (lb:ub)
                     class default
                         error stop vector % state % errmsg
@@ -154,16 +154,15 @@ submodule (vectors) vectors_int32_t_implementation
         end subroutine
 
 
-        module subroutine vector_int32_t_grow (vector, value)
+        module subroutine vector_int64_t_grow (vector, value)
             ! Synopsis: Doubles the vector size.
             type(vector_t), intent(inout), target :: vector
             integer(kind = int64):: lb
             integer(kind = int64):: ub
             integer(kind = int64):: bounds(0:1)
-            integer(kind = int32), intent(in) :: value
-            integer(kind = int32), allocatable :: array(:)
-            integer(kind = int32), pointer, contiguous :: ptr(:)
-
+            integer(kind = int64), intent(in) :: value
+            integer(kind = int64), allocatable :: array(:)
+            integer(kind = int64), pointer, contiguous :: ptr(:)
 
 
             lb = vector % begin % idx
@@ -177,11 +176,11 @@ submodule (vectors) vectors_int32_t_implementation
             associate (values => vector % array % values)
 
                 select type (values)
-                    type is ( integer(kind = int32) )
+                    type is ( integer(kind = int64) )
                         ptr => values(:)
                         call copy (array, ptr)
                     class default
-                        error stop "dynamic::vector.grow: unexpected error"
+                        error stop vector % state % errmsg
                 end select
 
             end associate
@@ -198,7 +197,7 @@ submodule (vectors) vectors_int32_t_implementation
             associate (values => vector % array % values)
 
                select type (values)
-                    type is ( integer(kind = int32) )
+                    type is ( integer(kind = int64) )
                        values = 0
                        call copy (values(lb:ub), array)
                     class default
@@ -207,29 +206,38 @@ submodule (vectors) vectors_int32_t_implementation
 
             end associate
 
+            ! TODO: check with valgrind if the array is automatically
+            !       deallocated upon return to caller. Would the
+            !       deallocation be handled by the OS rather than the
+            !       program in this case? Would not deallocating
+            !       explicitly result in a speed up.
+            ! TEST: outcome, array is automatically deallocated on return.
+            !       Did not check the runtime.
+!           call deallocator (array)
+
 
             return
-        end subroutine vector_int32_t_grow
+        end subroutine vector_int64_t_grow
 
 
-        module subroutine vector_int32_t_initializer (vector, value)
+        module subroutine vector_int64_t_initializer (vector, value)
             type(vector_t), intent(inout) :: vector
-            integer(kind = int32), intent(in) :: value
+            integer(kind = int64), intent(in) :: value
             call create (vector, value)
             return
         end subroutine
 
 
-        module subroutine vector_int32_t_create (vector, value)
+        module subroutine vector_int64_t_create (vector, value)
             ! Synopsis: Creates the first element in vector.
             type(vector_t), intent(inout) :: vector
             integer(kind = int64) :: bounds(0:1)
             integer(kind = int64), parameter :: lb = 0_int64
             integer(kind = int64), parameter :: ub = 8_int64
-            integer(kind = int32), intent(in) :: value
+            integer(kind = int64), intent(in) :: value
             integer(kind = int32) :: mstat
             character(len=*), parameter :: errmsg = &
-                & "dynamic::vector.error: container of 32-bit integers"
+                & "dynamic::vector.error: container of 64-bit integers"
 
 
 !           TODO: consider moving to utils of the vector class
@@ -252,7 +260,7 @@ submodule (vectors) vectors_int32_t_implementation
                      & values => vector % array % values)
 
                 select type (values)
-                    type is ( integer(kind = int32) )
+                    type is ( integer(kind = int64) )
                         values         = 0
                         values (avail) = value
                     class default
@@ -268,7 +276,7 @@ submodule (vectors) vectors_int32_t_implementation
 
 
             return
-        end subroutine vector_int32_t_create
+        end subroutine vector_int64_t_create
 
 
 end submodule
@@ -277,78 +285,3 @@ end submodule
 ! References:
 ! SJ Chapman, FORTRAN for Scientists and Engineers, fourth edition
 ! A Koenig and B Moo, Accelerated C++ Practical Programming by Example
-
-
-! The vector class presented by Koening and Moo inspired me to write my
-! own in fortran. I borrowed some of their ideas to implement it.
-
-
-! Comments:
-! A vector is a container that holds values of the same type. The
-! "default class" in select type constructs sometimes is used as a
-! mechanism that saves the user from catastrophy:
-! An array in Fortran can only hold values of the same type and so the
-! vector class which uses the Fortran array as the underlying data
-! structure to store values.
-
-
-! TODO:
-! [x] GUARD against indexing beyond the bounds of the vector
-! [x] CHECK procedures that attempt to use the vector (allocatable)
-!     components. Instantiating the vector might suffice to fix some.
-! [x] CHECK procedures that rely on the status value to make decisions.
-!     Note that the status component is now an allocatable, so that it
-!     is unsafe to check for its value since it could be unallocated.
-!     A possible solution is to check for the allocation status first,
-!     then check for the value.
-! [ ] PARTITION further into submodules. Idea: keep methods in a submodule
-!     and implementations in another. You will need another for utility
-!     procedures (for memory management).
-! [x] IMPLEMENT method that returns an iterator to values in the asymmetric
-!     range [begin, avail). Implement the iterator as a pointer.
-! [ ] IMPLEMENT a polymorphic push-back method (value is of type class(*)).
-!     Use a select type construct to determine what version
-!     (of "proxy" procedure) to call at runtime.
-!     Note: Most methods delegates work to a "proxy" of sorts. The method
-!     test_polymorphic_method() may be used as a template for other ones.
-! [ ] GUARD against using the vector to store integers of different types.
-!     If the user inserts a 32-bit integer it won't be able to insert
-!     a 64-bit integer unless the vector contents are cleared. A possible
-!     implementation is to check the allocation status of its counterpart
-!     upon calls to the push-back method.
-! [x] EXPERIMENT with dynamically allocated strings. Not fully supported
-!     by GNU Fortran Compiler. Cannot use them without disabling some
-!     compiler options I would like to retain.
-
-
-! Comments:
-! For now iterators are implemented as indexes.
-!
-
-
-! Comments on Procedures:
-!
-! subroutine vector_int32_t_create()
-! Allocates one more element on purpose to compute the size of the
-! vector via: (end - begin) as in c++.
-!
-!
-! function findloc_wrapper_method (self, value) result(idx)
-! lower and upper bounds (lb, ub) are chosen so that we do not include
-! the element pointed to by (end). It's a valid index but it should not
-! be referenced since it does not hold an actual value.
-!
-!
-! subroutine clear_method()
-! Placing the /avail/ iterator at the beginning is equivalent to
-! clearing the vector without deallocating memory. I have designed
-! the vector class thinking on how it will be used for keeping
-! track of neighbors. In that context it's convenient to
-! clear the vector without deallocating memory since it would
-! be expensive to have to grow the size of the vector over and
-! over again during the simulation. Why not use fixed-size arrays?
-! I have use them in the past worrying that I might have to allocate
-! more memory than actually needed to avoid exceding the array
-! bounds. Some systems might be more dynamic having particles with
-! far more neighbors than others. Vectors would come in handy for
-! such cases.
