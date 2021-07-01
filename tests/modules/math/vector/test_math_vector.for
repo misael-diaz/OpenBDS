ae097fbe153b41b79a1462fbfad2fe924449825b
misael-diaz <misael.diaz@upr.edu>
2021-07-01 18:37:17 -0400

    updates test to account for changes in vector.find

--- tests/modules/math/vector/test_math_vector.for
+++ tests/modules/math/vector/test_math_vector.for
@@ -181,11 +181,10 @@ program test_math_vector_class
     print *, "distance::min: ", dsqrt( minval(vector % v) )
     print *, "distance::max: ", dsqrt( maxval(vector % v) )
     print *, "vector::neighbors: ", neighbors % size ()
-    print *, "find: ", neighbors % find (n_int32)
 
 
     ! checks if the particle has included itself in the neighbor-list
-    if (neighbors % find (n_int32) /= 0) then
+    if (neighbors % find (n_int32) /= -1) then
         print *, "fail"
     else
         print *, "pass"
