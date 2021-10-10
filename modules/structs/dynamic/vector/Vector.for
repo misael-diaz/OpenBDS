2e232cb0bcb8497ec496ce6b0895af61b77a958a
misael-diaz <misael.diaz@upr.edu>
2021-10-10 17:56:16 -0400

    partitions grow subroutine for [vector] <int32_t>
    Comments:
    Applies to vectors of 32-bit integers `vector<int32_t>'.
    
    Partitions grow into `backup' and `restore' subroutines. The
    `backup' subroutine backs up the data contained in vector and the
    `restore' subroutine restores the data back into the vector whose
    size has been doubled.
    
    Resizing of the vector is also managed by the `restore' subroutine.

--- modules/structs/dynamic/vector/Vector.for
+++ modules/structs/dynamic/vector/Vector.for
@@ -244,6 +244,16 @@ module VectorClass
   end interface
 
 
+  interface backup
+      module procedure vector_int32_t_backup
+  end interface
+
+
+  interface restore
+      module procedure vector_int32_t_restore
+  end interface
+
+
 ! interface to_string
 !     module procedure to_string_int32
 !     module procedure to_string_int64
@@ -441,7 +451,7 @@ module VectorClass
 
     module subroutine vector_int32_t_grow (vector, value)
         ! Synopsis: Doubles the vector size.
-        type(vector_t), intent(inout), target :: vector
+        type(vector_t), intent(inout) :: vector
         integer(kind = int32), intent(in) :: value
     end subroutine
 
@@ -461,6 +471,19 @@ module VectorClass
     module subroutine vector_vector_t_grow (vector, value)
         type(vector_t), intent(inout) :: vector
         type(vector_t), intent(in) :: value
+    end subroutine
+
+
+    module subroutine vector_int32_t_backup (vector, array)
+        type(vector_t), intent(in) :: vector
+        integer(kind = int32), intent(inout), allocatable :: array(:)
+    end subroutine
+
+
+    module subroutine vector_int32_t_restore (vector, array, value)
+        type(vector_t), intent(inout) :: vector
+        integer(kind = int32), intent(in) :: array(:)
+        integer(kind = int32), intent(in) :: value
     end subroutine
 
 
