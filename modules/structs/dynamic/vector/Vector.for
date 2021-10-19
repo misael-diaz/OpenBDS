b4d91bd6b84cc1831e33c48dca3995f38c88e6e7
misael-diaz <misael.diaz@upr.edu>
2021-10-19 13:36:41 -0400

    implements array-constructor <int32_t> [vector]
    COMMENTS:
    The `fill' constructor was used as a `template' to implement it.
    
    The test shows that there are no differences between the stored
    values and the array. Valgrind reports that memory leaks are not
    possible.

--- modules/structs/dynamic/vector/Vector.for
+++ modules/structs/dynamic/vector/Vector.for
@@ -190,6 +190,7 @@ module VectorClass
 
   interface vector_t
       module procedure default_constructor
+      module procedure vector_int32_t_arrayConstructor
       module procedure vector_int32_t_fillConstructor
       module procedure vector_int64_t_fillConstructor
       module procedure vector_real64_t_fillConstructor
@@ -365,6 +366,12 @@ module VectorClass
     module function default_constructor () result(vector)
         ! Synopsis: Returns an empty vector
         type(vector_t), allocatable :: vector
+    end function
+
+
+    module function vector_int32_t_arrayConstructor (array) result(vec)
+        type(vector_t), allocatable :: vec
+        integer(kind = int32), intent(in) :: array(:)
     end function
 
 
