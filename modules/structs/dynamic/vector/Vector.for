9932d51f7e6dcb8c0331a441b5c60460afdffff7
misael-diaz <misael.diaz@upr.edu>
2021-10-19 14:17:17 -0400

    implements array-constructor <int64_t> [vector]
    COMMENTS:
    Commands used to generate code:
    
    cat Vector_int32_t_implementations.for | sed -n '31,127p' \
        | sed 's/int32_t/int64_t/' \
        | sed 's/integer(kind = int32),/integer(kind = int64),/' \
        | sed 's/value = 0/value = 0_int64/' \
        | sed 's/( integer(kind = int32) )/( integer(kind = int64) )/'\
        | sed 's/32-bit/64-bit/' | sed 's/_i32/_i64/'
    
    Code passes usual tests successfully.

--- modules/structs/dynamic/vector/Vector.for
+++ modules/structs/dynamic/vector/Vector.for
@@ -191,6 +191,7 @@ module VectorClass
   interface vector_t
       module procedure default_constructor
       module procedure vector_int32_t_arrayConstructor
+      module procedure vector_int64_t_arrayConstructor
       module procedure vector_int32_t_fillConstructor
       module procedure vector_int64_t_fillConstructor
       module procedure vector_real64_t_fillConstructor
@@ -372,6 +373,12 @@ module VectorClass
     module function vector_int32_t_arrayConstructor (array) result(vec)
         type(vector_t), allocatable :: vec
         integer(kind = int32), intent(in) :: array(:)
+    end function
+
+
+    module function vector_int64_t_arrayConstructor (array) result(vec)
+        type(vector_t), allocatable :: vec
+        integer(kind = int64), intent(in) :: array(:)
     end function
 
 
