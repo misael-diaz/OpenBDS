0c6fe2eec555e57ad34388b85139ffef3b589e38
misael-diaz <misael.diaz@upr.edu>
2021-10-19 15:51:28 -0400

    implements array-constructor <vector_t> [vector]
    COMMENTS:
    Commands used to generate code:
    
    cat Vector_int32_t_implementations.for | sed -n '31,127p' \
        | sed 's/int32_t/vector_t/'                           \
        | sed 's/integer(kind = int32),/type(vector_t),/'     \
        | sed 's/, parameter :: value = 0/ :: value/'         \
        | sed 's/( integer(kind = int32) )/(vector_t)/'       \
        | sed 's/32-bit integer/vector/' | sed 's/_i32/_vec/'
    
    Code passes tests successfully, including memory leak tests.

--- tests/modules/structs/dynamic/vector/test_vector.for
+++ tests/modules/structs/dynamic/vector/test_vector.for
@@ -47,19 +47,17 @@ module vector_class_tests
 
         subroutine test_vector_array_constructor ()
             ! tests creating vectors from arrays
-            type(vector_t), allocatable :: vector
-            type(vector_t), allocatable :: veci64
-            type(vector_t), allocatable :: vecr64
-            type(vector_t), allocatable :: vofvec
-            type(vector_t), allocatable :: aryvec(:)
-            integer(kind = int64) :: k
-            integer(kind = int32) :: i, j
-            integer(kind = int64) :: diff(3)
-            integer(kind = int64) :: diffs(2)
+            type(vector_t), allocatable :: vector     !! vector of ints
+            type(vector_t), allocatable :: veci64     !! vector of longs
+            type(vector_t), allocatable :: vecr64     !! vector of doubles
+            type(vector_t), allocatable :: vofvec     !! vector of vectors
+            type(vector_t), allocatable :: aryvec(:)  !! array of vectors
+            integer(kind = int64) :: j
+            integer(kind = int32) :: i, mstat
+            integer(kind = int64) :: diff(4)
             integer(kind = int64), parameter :: b = 0_int64, e = 63_int64
-            integer(kind = int64), parameter :: ary64(*) = [(k, k = b, e)]
+            integer(kind = int64), parameter :: ary64(*) = [(j, j = b, e)]
             integer(kind = int32), parameter :: array(*) = [(i, i = 0, 63)]
-            integer(kind = int32) :: mstat
             integer(kind = int32), parameter :: numel = &
                   & size(array = array, dim = 1, kind = int32)
 
@@ -69,9 +67,16 @@ module vector_class_tests
             if (mstat /= 0) error stop 'test-push-back: allocation error'
 
 
-            vector = vector_t (array)   !! creates vector from array
+            ! creates vectors from arrays
+            vector = vector_t (array)
             veci64 = vector_t (ary64)
             vecr64 = vector_t ( real(ary64, kind = real64) )
+
+            do i = 1, numel
+                aryvec(i) = veci64
+            end do
+
+            vofvec = vector_t (aryvec)
 
 
             ! checks for differences between stored and input values
@@ -105,13 +110,33 @@ module vector_class_tests
             end associate
 
 
+            diff(4) = 0_int64
+            associate (iter => vofvec % deref % it)
+                select type (iter)
+                    type is (vector_t)
+                        do i = 1, numel
+                            associate (it => iter(i) % deref % it)
+                                select type (it)
+                                    type is ( integer(kind = int64) )
+                                        diff(4) = diff(4) + sum(it - ary64)
+                                    class default
+                                        error stop 'test: unexpected error'
+                                    end select
+                            end associate
+                        end do
+                    class default
+                        error stop 'test-array-constructor: unexpected err'
+                end select
+            end associate
+
+
             write (*, '(A)', advance='no') &
                 & '[00] test-vector-array-constructor(): '
             if ( vector % size() /= int(numel, kind = int64) ) then
                 print *, 'FAIL'
             else if (diff(1) /= 0_int64 .or. diff(2) /= 0_int64) then
                 print *, 'FAIL'
-            else if (diff(3) /= 0_int64) then
+            else if (diff(3) /= 0_int64 .or. diff(4) /= 0_int64) then
                 print *, 'FAIL'
             else
                 print *, 'pass'
