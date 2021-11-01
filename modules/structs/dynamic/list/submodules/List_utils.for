9f72f2a0c25595df97e274927ed3aaf873f846ca
misael-diaz <misael.diaz@upr.edu>
2021-11-01 18:01:24 -0400

    caters empty links< > [list]
    COMMENTS:
    If the link is NULL, it makes sense to nullify the pointer to the
    node as it is done. If there's a node pointed to by the link, then
    the node pointer is associated to the linked node (as before). The
    change accounts for the possibility of an empty link< >.

--- modules/structs/dynamic/list/submodules/List_utils.for
+++ modules/structs/dynamic/list/submodules/List_utils.for
@@ -32,13 +32,19 @@ contains
       type(node_t), intent(inout), pointer :: node
       class(*), pointer :: h_node => null()
 
-      h_node => link % node % p
-      select type (h_node)
-          type is (node_t)
-              node => h_node
-          class default
-              error stop 'node<*data_t>.associate(): unexpected type error'
-      end select
+      if ( associated(link % node % p) ) then
+
+          h_node => link % node % p
+          select type (h_node)
+              type is (node_t)
+                  node => h_node
+              class default
+                  error stop 'node<*data_t>.associate(): unexpected type error'
+          end select
+
+      else
+          node => null()
+      end if
 
       h_node => null()
 
