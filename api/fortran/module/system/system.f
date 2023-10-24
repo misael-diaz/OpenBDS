41dc937766de47588f637c347ffe344a4699cd4a
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-10-24 12:29:34 -0400

    adds periodic boundary conditions code [FORTRAN]

---
+++ api/fortran/module/system/system.f
@@ -0,0 +1,87 @@
+      module system
+        use, intrinsic :: iso_fortran_env, only: real64
+        use :: constant, only: N => NUM_PARTICLES
+        use :: constant, only: L => LENGTH
+        use :: particle, only: particle_t
+        implicit none
+        private
+        public :: system__pbc
+
+        interface system__pbc
+          module procedure pbc
+        end interface
+
+      contains
+
+        pure subroutine update_position_vector_component (x)
+c         Synopsis:
+c         Applies periodic boundary conditions to a position vector component.
+          real(kind = real64), intent(inout) :: x(N)
+
+          x = x - anint(x / L) * L
+
+          return
+        end subroutine update_position_vector_component
+
+
+        pure subroutine apply_periodic_boundary_conditions (x, y, z)
+c         Synopsis:
+c         Applies periodic boundary conditions on the position vector components.
+          real(kind = real64), intent(inout) :: x(N)
+          real(kind = real64), intent(inout) :: y(N)
+          real(kind = real64), intent(inout) :: z(N)
+
+          call update_position_vector_component(x)
+          call update_position_vector_component(y)
+          call update_position_vector_component(z)
+
+          return
+        end subroutine apply_periodic_boundary_conditions
+
+
+        pure subroutine pbc (particles)
+c         Synopsis:
+c         Applies periodic boundary conditions on the position vectors of the particles.
+          class(particle_t), intent(inout) :: particles
+          real(kind = real64), pointer, contiguous :: x(:)
+          real(kind = real64), pointer, contiguous :: y(:)
+          real(kind = real64), pointer, contiguous :: z(:)
+
+          x => particles % x
+          y => particles % y
+          z => particles % z
+
+          call apply_periodic_boundary_conditions(x, y, z)
+
+          return
+        end subroutine pbc
+
+      end module system
+
+*   OpenBDS                                             October 24, 2023
+*
+*   source: api/fortran/module/system/system.f
+*   author: @misael-diaz
+*
+*   Synopsis:
+*   Implements methods for applying periodic boundary conditions.
+*
+*   Copyright (C) 2023 Misael Díaz-Maldonado
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*   References:
+*   [0] SJ Chapman, FORTRAN for Scientists and Engineers, 4th edition.
+*   [1] MP Allen and DJ Tildesley, Computer Simulation of Liquids.
+*   [2] S Kim and S Karrila, Microhydrodynamics.
